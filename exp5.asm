633da017806312cd0c1e5af4d02841255b3c2f1d
Gautham P R <gauthampr18@gmail.com>
2021-06-01 08:50:35 +0530

    Minor

--- exp5.asm
+++ exp5.asm
@@ -1,106 +1,125 @@
-data SEGMENT
-	msg1   DB 0ah, 0dh, "Enter how many: $"
-	msg2   DB 0ah, 0dh, "Enter the number: $"
-    msg3   DB 0ah, 0dh, "Enter Sub-string to replace with: $"
-	affirm DB 0ah, 0dh, "Sub-String$"
-	deny   DB 0ah, 0dh , "Not a Sub-string$"
-	str    DB 2Fh dup('$')
-	subString DB 2Fh dup('$')
-    replaceStr DB 2Fh dup('$')
-    finalMsg DB 0ah, 0dh, "Output: "
-    finalStr DB 4Fh dup('$')
-	lenStr DW 0000h
-    lenSubStr DW 0000h
-    lenReplaceStr DW 0000h
-    index DW 0000h
-	eos    DB '$'
-data ENDS
-code SEGMENT
-	                 ASSUME CS:code, DS: data
-	start:           MOV    AX, data
-	                 MOV    DS, AX
-	                 MOV    CX, 0000h
-	                 LEA    DX, msg1
-	                 MOV    AH, 09h
-	                 INT    21h
-	                 LEA    DI, str
-	loopStrInput:    MOV    AH, 01h
-	                 INT    21h
-	                 CMP    AL, 0dh
-	                 JE     subStrInput
-	                 INC    CX
-	                 MOV    [DI], AL
-	                 INC    DI
-	                 JMP    loopStrInput
-	subStrInput:     MOV    lenStr, CX
-	                 LEA    DX, msg2
-	                 MOV    AH, 09h
-	                 INT    21h
-	                 LEA    DI, subString
-	loopSubStrInput: MOV    AH, 01h
-	                 INT    21h
-	                 CMP    AL, 0dh
-	                 JE     replaceStrInput
-                     INC    lenSubStr
-	                 MOV    [DI], AL
-	                 INC    DI
-	                 JMP    loopSubStrInput
-    replaceStrInput: LEA    DX, msg3
-	                 MOV    AH, 09h
-	                 INT    21h
-	                 LEA    DI, replaceStr
-	loopReplaceStrInput: MOV    AH, 01h
-	                 INT    21h
-	                 CMP    AL, 0dh
-	                 JE     checkStart
-                     INC    lenReplaceStr
-	                 MOV    [DI], AL
-	                 INC    DI
-	                 JMP    loopReplaceStrInput
-	checkStart:      LEA    SI, subString
-	                 LEA    DI, str
-	                 MOV    AX, lenStr
-	                 SUB    AX, CX
-	                 ADD    DI, AX
-	                 MOV    DX, [DI]
-	                 MOV    BX, [SI]
-	                 CMP    BL, DL
-	                 JE     checkConsecutive
-                     LEA    DI, finalStr
-                     MOV    AX, index
-	                 ADD    DI, AX
-                     MOV    [DI], DX
-                     INC    index
-	                 LOOP   checkStart
-	printDeny:       LEA    DX, finalMsg
-	                 JMP    exit
-	checkConsecutive:MOV    DX, [DI]
-	                 MOV    BX, [SI]
-	                 INC    DI
-	                 INC    SI
-	                 CMP    BL, '$'
-	                 JE     startReplace
-	                 CMP    BL, DL
-	                 JE     checkConsecutive
-                     DEC    CX
-	                 JMP    checkStart
-    startReplace:    LEA    SI, replaceStr
-                     LEA    DI, finalStr
-                     MOV    AX, lenStr
-	                 SUB    AX, CX
-	                 ADD    DI, AX
-                     SUB    CX, lenSubStr
-	doReplace:       MOV    AX, [SI]
-                     MOV    [DI], AX
-                     CMP    [SI], '$'
-                     JE     checkStart
-                     INC    SI
-                     INC    index
-                     INC    DI
-	                 JMP    doReplace
-	exit:            MOV    AH, 09h
-	                 INT    21h
-	                 MOV    AH, 4Ch
-	                 INT    21h
-code ENDS
-END start
+data segment
+	countMsg DB 0ah, 0dh, "Enter count(max 5): $"
+	finalMsg DB 0ah, 0dh, "Sorted: $"
+	searchMsg DB 0ah, 0dh, "Enter number to search: $"
+	affirmMsg DB 0ah, 0dh, "The number is found at index: $"
+	denyMsg DB 0ah, 0dh, "The number is not found$"
+	searchNum DW ?
+	space DB " $"
+	opr1 db 0ah,0dh,"Enter number: $"
+	count dw ?
+	tempCount dw ?
+	numbers DW 4Fh dup(0000h)
+	num dw ?
+	num1 dw ?
+data ends
+
+display macro msg
+	mov dx,offset msg
+	mov ah,09h
+	int 21h
+endm
+
+read macro num
+	local L1,exit
+	mov ah,01h
+	int 21h
+	mov ah,00h
+	sub ax,0030h
+	mov num,ax
+	L1:mov ah,01h
+	int 21h
+	cmp al,0dh
+	je exit
+	mov ah,00h
+	sub ax,0030h
+	mov num1,ax
+	mov ax,num
+	mov bx,000ah
+	mul bx
+	add ax,num1
+	mov num,ax
+	jmp L1
+	exit:nop
+endm
+print macro num
+    local L2,L3
+	mov ax,num
+	mov cx,0000h
+	L2: mov dx,0000h
+	mov bx,000ah
+	div bx
+	push dx
+	inc cx
+	cmp ax,0000h
+	jne  L2
+	L3: pop dx
+	add dx,0030h
+	mov ah,02h
+	int 21h
+	loop L3
+endm
+code segment
+assume cs:code,ds:data
+	start: 			mov ax,data
+					mov ds,ax
+					display countMsg
+					read count
+					MOV CX, count
+					MOV tempCount, CX
+					MOV DI, 0000h
+	readNumbers: 	display opr1
+					read numbers[DI]
+					INC DI
+					INC DI
+					LOOP readNumbers
+	fullTraverse:	MOV DI, 0000h
+					DEC tempCount
+					MOV CX, count
+					DEC CX
+					CMP tempCount, 0000h
+					JE exit
+	movePointer:	CMP CX, 0000h
+					JE	fullTraverse
+					MOV AX, numbers[DI]
+					MOV BX, numbers[DI+2]
+					DEC CX
+					INC DI
+					INC DI
+					CMP AX, BX
+					JLE movePointer
+					MOV numbers[DI], AX
+					MOV numbers[DI-2], BX
+					JMP movePointer
+	exit:			MOV CX, count
+					MOV DI, 0000h
+					display finalMsg
+	printNumber:	MOV tempCount, CX
+					print numbers[DI]
+					display space
+					MOV CX, tempCount
+					INC DI
+					INC DI
+					LOOP printNumber
+	searchNumber:	display searchMsg
+					read searchNum
+					MOV CX, count
+					MOV DI, 0000h
+	traverseArray:	MOV AX, searchNum
+					CMP AX, numbers[DI]
+					JE affirm
+					INC DI
+					INC DI
+					LOOP traverseArray
+					display denyMsg
+					mov ah,4ch
+					int 21h
+	affirm:			display affirmMsg
+					MOV AX, DI
+					MOV BL, 0002h
+					DIV BL
+					print AX
+					mov ah,4ch
+					int 21h
+code ends
+end start
